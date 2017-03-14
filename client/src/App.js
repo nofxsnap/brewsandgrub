3ff4f94a5f53ccb899f46a5721f8fc43cb365a07
John Siladie <john.siladie@gmail.com>
2017-03-14 15:22:25 -0600

    Add temp brews and grub image.

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Brews and Grub</h2>
         </div>
         <p className="App-intro">
+          <img src="https://s3-us-west-2.amazonaws.com/brewsandgrub/brews_and_grub_temp.jpg" alt="brews and grub" />
+          <br />
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
