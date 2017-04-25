6959e053bec178d1c00efac8d8f1d7d5b74abce9
hrrf <nathan.lamb@gmail.com>
2017-04-25 15:26:29 -0600

    forgot to add schema
    related to adding possibility to blank a food truck

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420171243) do
+ActiveRecord::Schema.define(version: 20170425170815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170420171243) do
     t.string   "event_hours"
     t.string   "schedule_scan_pattern"
     t.string   "schedule_gsub_pattern"
+    t.boolean  "is_restaurant"
     t.index ["contact_id"], name: "index_breweries_on_contact_id", using: :btree
     t.index ["menu_id"], name: "index_breweries_on_menu_id", using: :btree
   end
